a7b172581a900e2fdaefe8ef32506ef6107bee3a
Nitsofts Inc <100211678+nitsofts@users.noreply.github.com>
2023-09-28 16:17:07 +1000

    Update app.py
    Added Buy Stock

--- app.py
+++ app.py
@@ -17,5 +17,40 @@ def add_numbers():
 
     return jsonify(response)
 
+@app.route('/buy', methods=['POST'])
+def buy():
+    data = request.get_json()
+    units = float(data.get('units', 0))
+    buying_price = float(data.get('buying_price', 0))
+
+    share_amount = units * buying_price
+    sebon_commission = (0.015 / 100) * share_amount
+
+    if share_amount <= 50000:
+        broker_commission = max(0.004 * share_amount, 10)
+    elif 50001 <= share_amount <= 500000:
+        broker_commission = 0.0037 * share_amount
+    elif 500001 <= share_amount <= 2000000:
+        broker_commission = 0.0034 * share_amount
+    elif 2000001 <= share_amount <= 10000000:
+        broker_commission = 0.003 * share_amount
+    else:
+        broker_commission = 0.0027 * share_amount
+
+    dp_fee = 25
+    total_paying_amount = share_amount + sebon_commission + broker_commission + dp_fee
+    cost_per_share = total_paying_amount / units
+
+    response_list = [
+        {'Share Amount': share_amount},
+        {'SEBON Commission': sebon_commission},
+        {'Broker Commission': broker_commission},
+        {'DP Fee': dp_fee},
+        {'Cost Per Share': cost_per_share},
+        {'Total Paying Amount': total_paying_amount}
+    ]
+
+    return jsonify(response_list)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
