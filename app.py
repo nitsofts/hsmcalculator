10aa27e50a25fb69f31c5fcfa8518213aa9dfb86
Nitsofts Inc <100211678+nitsofts@users.noreply.github.com>
2023-09-28 16:38:20 +1000

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def buy():
     buying_price = float(request.args.get('buying_price', 0))
 
     share_amount = round(units * buying_price, 2)
-    sebon_commission = round((0.015 / 100) * share_amount, 2)
+    sebon_fee = round((0.015 / 100) * share_amount, 2)
 
     if share_amount <= 50000:
         broker_commission = round(max(0.004 * share_amount, 10), 2)
@@ -36,16 +36,17 @@ def buy():
     else:
         broker_commission = round(0.0027 * share_amount, 2)
 
-    dp_fee = 25
-    total_paying_amount = round(share_amount + sebon_commission + broker_commission + dp_fee, 2)
+    dp_charge = 25
+    total_paying_amount = round(share_amount + sebon_fee + broker_commission + dp_charge, 2)
     cost_per_share = round(total_paying_amount / units, 2)
+    price_per_share = round(buying_price + (broker_commission / units), 2)
 
     # Check if the share amount and other fields are whole numbers
     if share_amount.is_integer():
         share_amount = int(share_amount)
     
-    if sebon_commission.is_integer():
-        sebon_commission = int(sebon_commission)
+    if sebon_fee.is_integer():
+        sebon_fee = int(sebon_fee)
     
     if broker_commission.is_integer():
         broker_commission = int(broker_commission)
@@ -55,14 +56,21 @@ def buy():
     
     if cost_per_share.is_integer():
         cost_per_share = int(cost_per_share)
+    
+    if price_per_share.is_integer():
+        price_per_share = int(price_per_share)
+
+    # Calculate the total charges
+    total_charges = sebon_fee + broker_commission + dp_charge
 
     response_dict = {
         'Share Amount': share_amount,
-        'SEBON Commission': sebon_commission,
+        'Sebon Fee': sebon_fee,
         'Broker Commission': broker_commission,
-        'DP Fee': dp_fee,
-        'Cost Per Share': cost_per_share,
-        'Total Paying Amount': total_paying_amount
+        'DP Charge': dp_charge,
+        'Price Per Share': price_per_share,
+        'Total Charges': total_charges,
+        'Payable Amount': total_paying_amount
     }
 
     return jsonify(response_dict)
